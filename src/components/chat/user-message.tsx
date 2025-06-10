298cdc59110e500caa9602eac673d3c727935a85
Hanif Ali <alihanif016@gmail.com>
2025-06-10 00:27:29 -0400

    add basic chatbox UI

--- src/components/chat/user-message.tsx
+++ src/components/chat/user-message.tsx
@@ -1,9 +1,10 @@
 import { Doc } from "../../../convex/_generated/dataModel";
 
 export function UserMessage({ message }: { message: Doc<"messages"> }) {
+	// TODO: white glow around the border
   return (
     <div className="flex justify-end">
-      <div className="bg-gray-700 rounded-lg p-4 max-w-xs">
+      <div className="rounded-xl p-4 max-w-xs bg-secondary/50 border-secondary/50">
         <p className="text-sm">{message.content}</p>
       </div>
     </div>
