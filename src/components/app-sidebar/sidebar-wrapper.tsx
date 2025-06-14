2633fe936683dfaf818340353a1f30e098fe0a21
Hanif Ali <alihanif016@gmail.com>
2025-06-14 00:16:20 -0400

    fix sidebar

--- src/components/app-sidebar/sidebar-wrapper.tsx
+++ src/components/app-sidebar/sidebar-wrapper.tsx
@@ -5,11 +5,6 @@ import { AppSidebar } from "@/components/app-sidebar/app-sidebar";
 import { FloatingButtons } from "@/components/floating-buttons";
 import { Preloaded } from "convex/react";
 import { api } from "../../../convex/_generated/api";
-import { 
-  ResizablePanelGroup, 
-  ResizablePanel, 
-  ResizableHandle 
-} from "@/components/ui/resizable";
 
 export function SidebarWrapper({ 
   children,
@@ -22,23 +17,8 @@ export function SidebarWrapper({
     <div className="flex h-screen text-white">
       <SidebarProvider>
         <FloatingButtons />
-        <ResizablePanelGroup 
-          direction="horizontal" 
-          className="h-full min-h-screen"
-        >
-          <ResizablePanel 
-            defaultSize={20} 
-            minSize={15} 
-            maxSize={35}
-            className="min-w-[200px]"
-          >
-            <AppSidebar preloadedThreads={preloadedThreads} />
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel defaultSize={80}>
-            {children}
-          </ResizablePanel>
-        </ResizablePanelGroup>
+        <AppSidebar preloadedThreads={preloadedThreads} />
+        {children}
       </SidebarProvider>
     </div>
   );
